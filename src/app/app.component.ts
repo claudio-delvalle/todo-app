5d4a75d5b4387e431874b93e005131b362eafe77
Pablo <p.camacho@globant.com>
2024-01-16 15:10:35 -0300

    Reorganizo estructura de carpetas y creo todo component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './todos/pages/home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
